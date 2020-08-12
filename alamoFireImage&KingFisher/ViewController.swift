f169047f330ae8323537742387c94ecf30e2b19e
aly fawzy <alyfawzy098@gmail.com>
2020-08-12 02:37:23 +0200

    first commit

--- alamoFireImage&KingFisher/ViewController.swift
+++ alamoFireImage&KingFisher/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AlamofireImage
+import Kingfisher
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    let url = URL(string:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRa_NE7cHQkudVWUIB5nw6h025k-Ootj3n3xzJIHNC1UmL_9mDj")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    //MARK:- IBAction
+    @IBAction func getImgeBtnAction(_ sender: UIButton) {
+       if sender.tag == 0{
+           self.getImage()
+        }else if sender.tag == 1{
+            alamoFireGetImage()
+        }else{
+            kingFisherGetImage()
+        }
+        
+        
+    }
+    
+    //MARK:- Helper function
+    func getImage()  {
+        let data = try! Data(contentsOf: url!)
+        self.img.image = UIImage(data: data)
+    }
+    func alamoFireGetImage()  {
+        let filter = AspectScaledToFillSizeWithRoundedCornersFilter(size: img.frame.size, radius: 30)
+        
+        img.af.setImage(
+            withURL: url!,
+            filter: filter,
+            imageTransition: .flipFromTop(0.5))
+    }
+    func kingFisherGetImage()  {
+        
+        self.img.kf.setImage(with: url, options: [.transition(.flipFromBottom(0.5))])
+    }
+    
 }
 
